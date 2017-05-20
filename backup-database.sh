97c89efcc76c1a883e15449e24a37e546c79cdf4
jaaaco <jakub@wietrzyk.com>
2017-05-20 20:33:42 +0200

    bugfix

--- backup-database.sh
+++ backup-database.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
-mysqldump -u $USERNAME -$PASSWORD -h mysql $DATABASE > database.sql
+mysqldump -u $MYSQL_USERNAME -$MYSQL_PASSWORD -h mysql $MYSQL_DATABASE > database.sql
 gzip database.sql
-aws s3api put-object --bucket $BUCKET --key $PREFIX/database.sql.gz --body database.sql.gz
+aws s3api put-object --bucket $AWS_BUCKET --key $PREFIX/database.sql.gz --body database.sql.gz
 rm database.sql.gz
